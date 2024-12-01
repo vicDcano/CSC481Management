c40b90bf87bd962ba63df0070485a56c580ac990
John <83319749+glockiicodes@users.noreply.github.com>
2024-11-30 17:19:50 -0800

    Create DatabaseConn.java
    Separated db connection

---
+++ src/DatabaseConn.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class DatabaseConn {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/decentbuy2";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "481jfortin1"; //changes this to your pass
+
+    public Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error connecting to the database");
+        }
+    }
+}
